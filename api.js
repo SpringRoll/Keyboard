d1ac328c780e8aff7bfb62dc66119ea48bb7414a
andrewstart <andrewstart@cloudkid.com>
2015-05-16 22:17:03 -0400

    Auto-generated commit

---
+++ api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "cloudkid.Keyboard",
+        "cloudkid.KeyboardPlugin",
+        "cloudkid.USLocale"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
