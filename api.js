0118c6c7a224a7192d7fb5ad09aac4d6adbfe35d
Andrew Start <andrewstart@cloudkid.com>
2015-05-20 10:24:20 -0400

    Auto-generated commit

--- api.js
+++ api.js
@@ -1,9 +1,9 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "cloudkid.Keyboard",
-        "cloudkid.KeyboardPlugin",
-        "cloudkid.USLocale"
+        "springroll.Keyboard",
+        "springroll.KeyboardPlugin",
+        "springroll.USLocale"
     ],
     "modules": [],
     "allModules": []
